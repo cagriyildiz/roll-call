c00299fdf5022985a5e85602decd037ddb894c8e
cagriyildiz <cagriyild@gmail.com>
2021-01-15 23:07:24 +0300

    Setup bottom navigation

--- app/src/main/java/com/jurengis/rollcall/ui/RollCallActivity.kt
+++ app/src/main/java/com/jurengis/rollcall/ui/RollCallActivity.kt
@@ -1,13 +1,30 @@
 package com.jurengis.rollcall.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.jurengis.rollcall.R
+import kotlinx.android.synthetic.main.activity_rollcall.*
 
 class RollCallActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rollcall)
+
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        bottomNavigationMenu.setupWithNavController(rollCallNavHostFragment.findNavController())
+
+        rollCallNavHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.loginFragment -> bottomNavigationMenu.visibility = View.GONE
+                }
+            }
     }
 }
