f14e6c1cab96054a0279d587a00047d21829cb27
cagriyildiz <cagriyild@gmail.com>
2021-01-15 22:39:58 +0300

    Create empty login fragment

---
+++ app/src/main/java/com/jurengis/rollcall/ui/RollCallActivity.kt
@@ -0,0 +1,13 @@
+package com.jurengis.rollcall.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jurengis.rollcall.R
+
+class RollCallActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rollcall)
+    }
+}
---
+++ app/src/main/java/com/jurengis/rollcall/ui/fragments/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.jurengis.rollcall.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.jurengis.rollcall.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+}
