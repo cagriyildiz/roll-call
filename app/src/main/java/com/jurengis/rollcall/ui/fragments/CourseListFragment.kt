33b94c08320e501405fa6a83740f366c4c7af7d7
cagriyildiz <cagriyild@gmail.com>
2021-01-18 23:39:00 +0300

    Request location permission from the user
    BLE needs location permissions

--- app/src/main/java/com/jurengis/rollcall/ui/fragments/CourseListFragment.kt
+++ app/src/main/java/com/jurengis/rollcall/ui/fragments/CourseListFragment.kt
@@ -1,7 +1,11 @@
 package com.jurengis.rollcall.ui.fragments
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -20,6 +24,10 @@ class CourseListFragment : Fragment(R.layout.fragment_course_list) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        requestLocationPermission()
+    }
+
+    private fun observeCourses() {
         viewModel.beacons.observe(viewLifecycleOwner, { courseList ->
             courseList?.let {
                 courseListAdapter.differ.submitList(courseList)
@@ -35,7 +43,53 @@ class CourseListFragment : Fragment(R.layout.fragment_course_list) {
         }
     }
 
+    private fun requestLocationPermission() {
+        if (allPermissionsGranted()) {
+            observeCourses()
+        } else {
+            requestPermissions(
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all { permission ->
+        return if (permission != null) {
+            ContextCompat.checkSelfPermission(
+                requireContext(), permission
+            ) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                observeCourses()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Location permission is needed for this app to be work properly.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                requireActivity().finish()
+            }
+        }
+    }
+
     companion object {
         private const val TAG = "AvailableCourses"
+        private const val REQUEST_CODE_PERMISSIONS = 11
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            } else { null })
     }
 }
