e7863e441b8a5cb8469889081cdf871e77a39bfa
cagriyildiz <cagriyild@gmail.com>
2021-01-21 00:29:23 +0300

    Provide Cloud Firestore instance as singleton

--- app/src/main/java/com/jurengis/rollcall/di/AppModule.kt
+++ app/src/main/java/com/jurengis/rollcall/di/AppModule.kt
@@ -2,6 +2,8 @@ package com.jurengis.rollcall.di
 
 import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,6 +20,10 @@ object AppModule {
     @Singleton
     @Provides
     fun provideFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Singleton
+    @Provides
+    fun provideFirebaseFirestore() = Firebase.firestore
 
     @Singleton
     @Provides
