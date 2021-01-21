c8eee03e6f6d0547bc6a3dde208e5b584d270505
cagriyildiz <cagriyild@gmail.com>
2021-01-21 20:57:02 +0300

    Create course enroll fragment

--- app/src/main/java/com/jurengis/rollcall/model/Course.kt
+++ app/src/main/java/com/jurengis/rollcall/model/Course.kt
@@ -1,8 +1,10 @@
 package com.jurengis.rollcall.model
 
+import java.io.Serializable
+
 data class Course(
     var id: String = "",
     val courseName: String = "",
     val classRoom: String = "",
     val instructor: String = ""
-)
+) : Serializable
---
+++ app/src/main/java/com/jurengis/rollcall/ui/fragments/CourseEnrollFragment.kt
@@ -0,0 +1,13 @@
+package com.jurengis.rollcall.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.jurengis.rollcall.R
+
+class CourseEnrollFragment : Fragment(R.layout.fragment_course_enroll) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
