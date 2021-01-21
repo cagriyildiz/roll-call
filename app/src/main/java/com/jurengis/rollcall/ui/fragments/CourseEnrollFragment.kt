9a7c9f132ed3ee9f674fae42aa952d4765bc1ea6
cagriyildiz <cagriyild@gmail.com>
2021-01-21 21:37:01 +0300

    Create basic view of course enroll fragment

--- app/src/main/java/com/jurengis/rollcall/ui/fragments/CourseEnrollFragment.kt
+++ app/src/main/java/com/jurengis/rollcall/ui/fragments/CourseEnrollFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.jurengis.rollcall.R
+import kotlinx.android.synthetic.main.fragment_course_enroll.*
 
 class CourseEnrollFragment : Fragment(R.layout.fragment_course_enroll) {
 
@@ -13,5 +14,7 @@ class CourseEnrollFragment : Fragment(R.layout.fragment_course_enroll) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val course = args.course
+        tvEnrollCourseName.text = course.courseName
+        tvEnrollInstructorName.text = course.instructor
     }
 }
